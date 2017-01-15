28408548e34df27716b936ccd76bad8456db1e57
reatgcr0wn <reatgcr0wn@gmail.com>
2017-01-16 06:17:33 +0900

    update

--- web/js/script.js
+++ web/js/script.js
@@ -1,8 +1,3 @@
-var slider = $('#slider').unslider({
-  delay: 300000
-});
-
-
 function reload(){
   location.reload();
 }
@@ -21,7 +16,7 @@ function getContent(timestamp) {
             // $('#response').html(obj.data_from_file);
             getContent(obj.timestamp);
             var obj = jQuery.parseJSON(obj.data_from_file);
-            changeAdvertising(obj.interest);
+            changeAdvertising(obj);
         }
     });
 }
@@ -30,35 +25,35 @@ $(function() {
     getContent();
 });
 
-function changeAdvertising(interest) {
-    console.log(interest);
-    switch (interest) {
-        case '1':
-            slider.unslider('animate:0');
-            break;
-        case '2':
-            slider.unslider('animate:1');
-            break;
-        case '3':
-            slider.unslider('animate:2');
-            break;
-        case '4':
-            slider.unslider('animate:3');
-            break;
-        case '5':
-            slider.unslider('animate:4');
-            break;
-        case '6':
-            slider.unslider('animate:5');
-            break;
-        case '7':
-            slider.unslider('animate:6');
-            break;
-        case '8':
-            slider.unslider('animate:7');
-            break;
-        default:
-    }
+function changeAdvertising(data) {
+    console.log(data);
+    // switch (interest) {
+    //     case '1':
+    //         slider.unslider('animate:0');
+    //         break;
+    //     case '2':
+    //         slider.unslider('animate:1');
+    //         break;
+    //     case '3':
+    //         slider.unslider('animate:2');
+    //         break;
+    //     case '4':
+    //         slider.unslider('animate:3');
+    //         break;
+    //     case '5':
+    //         slider.unslider('animate:4');
+    //         break;
+    //     case '6':
+    //         slider.unslider('animate:5');
+    //         break;
+    //     case '7':
+    //         slider.unslider('animate:6');
+    //         break;
+    //     case '8':
+    //         slider.unslider('animate:7');
+    //         break;
+    //     default:
+    // }
 
     // switch (interest) {
     //   case 'a':
